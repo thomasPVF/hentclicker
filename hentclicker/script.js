5944c696fe34e48e77c9aa3927488cb4078bb01e
thomasPVF <programmincl@gmail.com>
2025-06-03 20:20:06 -0400

    primer commit de mi clicker

---
+++ hentclicker/script.js
@@ -0,0 +1,26 @@
+let count = 0;
+let perClick = 1;
+let upgradeCost = 100;
+
+const counter = document.getElementById("counter");
+const clicker = document.getElementById("clicker");
+const upgrade = document.getElementById("upgrade");
+
+clicker.onclick = () => {
+  count += perClick;
+  update();
+};
+
+upgrade.onclick = () => {
+  if (count >= upgradeCost) {
+    count -= upgradeCost;
+    perClick += 1;
+    upgradeCost *= 2;
+    upgrade.textContent = `+1 por click (${upgradeCost})`;
+    update();
+  }
+};
+
+function update() {
+  counter.textContent = count;
+}
